b8473fe315d7ec8a3fc33e0a9627fac8325fcec0
Martin Chau <martinchaunz@outlook.com>
2019-01-30 15:31:35 +1300

    Relationship sorting and tests

--- src/find/apply-sorts.ts
+++ src/find/apply-sorts.ts
@@ -1,5 +1,5 @@
 import { QueryBuilder } from "knex";
-import { StrictModel, RelType } from "../models/model-interface";
+import { StrictModels, RelType, StrictModel } from "../models/model-interface";
 import { Error as APIError, FindQuery, Sort, FieldSort } from "json-api";
 
 /**
@@ -8,40 +8,111 @@ import { Error as APIError, FindQuery, Sort, FieldSort } from "json-api";
  */
 export default function applySorts(
   query: QueryBuilder,
-  model: StrictModel,
+  models: StrictModels,
+  primaryType: string,
   sorts: FindQuery['sort']
 ): void {
+
   if (sorts == null) return;
 
-  validateSorts(model, sorts);
+  validateSorts(models, primaryType, sorts);
 
   // Cast to FieldSort[] is checked by validateSorts()
+  const relationshipSortFields = [] as string[];
   for (const { field, direction } of sorts as FieldSort[]) {
-    const key = field === 'id' ? model.idKey : field;
-    query.orderBy(`${model.table}.${key}`, direction.toLowerCase());
+    if(!getValidKeys(models[primaryType]).includes(field)){
+      // must be field of relationship
+      relationshipSortFields.push(field); // track relationship sorts for joining
+      query.orderBy(field,direction.toLowerCase());
+    } else {
+      const key = field === 'id' ? models[primaryType].idKey : field;
+      query.orderBy(`${models[primaryType].table}.${key}`, direction.toLowerCase());
+    };
+    
   }
+  joinRequiredRelationshipsForSorts (query, models, primaryType, relationshipSortFields)
 };
 
-/**
- * Catch sorts of keys not in the model. Important for security as this prevents sorting by hidden,
- * private attributes, potentially resulting in data leakage.
- */
-function validateSorts(
-  model: StrictModel,
-  sorts: Sort[]
-): void {
-  const validKeys = [
+function getRelationshipTypeFromKey(model : StrictModel, key: string){
+  const relationship = model.relationships.find( relationship => relationship.key === key);
+   if(!relationship){
+    throw new APIError({
+      status: 400,
+      title: 'Invalid sort',
+      detail: `The relationship '${key}' does not exist on this model.'`
+    });
+  }
+  return relationship.type
+}
+
+function getValidKeys(model : StrictModel){
+  return [
     'id',
     ...model.attrs.map(attr => attr.key),
     ...model.relationships
       .filter(rel => rel.relType === RelType.MANY_TO_ONE)
       .map(rel => rel.key)
   ]
-  
-  const invalidSorts = sorts.filter(
-    sort => !('field' in sort) || !validKeys.includes(sort.field)
-  );
+}
 
+function joinRequiredRelationshipsForSorts(
+  query: QueryBuilder,
+  models: StrictModels,
+  primaryType: string,
+  sortFields: string[]
+){  
+  // join relationships that are referenced by sorts
+  const relationshipKeys = [...new Set(sortFields.map(field => field.substring(0,field.indexOf('.'))))]
+  relationshipKeys.forEach( relationshipKey => {
+    const relationshipType = getRelationshipTypeFromKey(models[primaryType], relationshipKey);
+    const relationshipTable = `${models[relationshipType].table}`;
+    const localFK = `${models[primaryType].table}.${relationshipKey}`;
+    const foreignPK = `${models[relationshipType].table}.${models[relationshipType].idKey}`;
+    query.leftJoin(relationshipTable, localFK, foreignPK);
+  })
+  // group by sorted relationship fields
+  sortFields.forEach(field => {
+    query.groupBy(field);
+  })
+}
+
+/**
+ * Catch sorts of keys not in the model. Important for security as this prevents sorting by hidden,
+ * private attributes, potentially resulting in data leakage.
+ */
+function validateSorts(
+  models: StrictModels,
+  primaryType: string,
+  sorts: Sort[]
+): void {
+  const validKeys = getValidKeys(models[primaryType]);
+  const invalidSorts = sorts.filter( sort => {
+    
+    if(!('field' in sort)){
+      return true;
+    } else if(!validKeys.includes(sort.field)){
+      
+      // not direct field match, check for relationship match
+      const relationshipKey = sort.field.substring(0,sort.field.indexOf('.'));
+      const relationshipField = sort.field.substring(sort.field.indexOf('.')+1);
+      // check if valid keys contains relationship name, 
+      if (!validKeys.includes(relationshipKey)){
+        return true
+      }
+      
+      // validate relationship field
+      const relationshipType = getRelationshipTypeFromKey(models[primaryType], relationshipKey);
+      let validRelationshipKeys;
+      if(relationshipType){
+        validRelationshipKeys = getValidKeys(models[relationshipType]);
+      }
+      
+      return !validRelationshipKeys.includes(relationshipField)
+    } else {
+      return false;
+    }
+  });
+  
   if (invalidSorts.length > 0) {
     throw invalidSorts.map(sort => new APIError({
       status: 400,
--- src/knex-adapter.ts
+++ src/knex-adapter.ts
@@ -92,7 +92,8 @@ export default class KnexAdapter implements Adapter<typeof KnexAdapter> {
 
     applyFieldFilters(kq, model, selectedFields);
     joinToManyRelationships(kq, model, selectedFields);
-    applySorts(kq, model, query.sort);
+
+    applySorts(kq, this.models, query.type, query.sort);
 
     let records: any[];
     let included: ReturnedResource[];
